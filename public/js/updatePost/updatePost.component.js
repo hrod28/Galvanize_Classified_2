c9bba4ea43d8fd15fb2e295561ea926745f9dd5c
Heidi Rodriguez <hrod2645@gmail.com>
2017-03-12 09:41:34 -0600

    add oninit and update posts functions

--- public/js/updatePost/updatePost.component.js
+++ public/js/updatePost/updatePost.component.js
@@ -1,24 +1,31 @@
 "use strict";
 (function() {
-  angular.module('app')
-    .component('updateAd', {
-      templateUrl: 'js/patch-ad/patch-ad.template.html',
+  angular.module('updatePost.component', ['ui.router'])
+    .component('updatePost', {
+      templateUrl: 'js/updatePost/updatePost.template.html',
       conrollerAs: 'model',
       controller: controller
     });
 
-    controller.$inject = ["$http", "$state", "$stateParams"];
+    controller.$inject = ["$http", "$state", "$stateParams", "$state", "$location"];
+
     function controller($http, $state, $stateParams) {
       const model = this;
-      model.newPost = $stateParams.newPost;
-      // model.$onInit = function() {
-      // };
 
-      model.updateAd = function() {
-        $http.patch(`/classifieds/${model.newPost.id}`, model.newPost)
-          .then((result) => {
-          $state.go("display");
-        })
-      };
+      model.$onInit = onInit;
+      model.updatePost = updatePost;
+      model.deletePost = deletePost;
+
+    function onInit(){
+      $http.get(`/api/classifieds/${stateParams.id}`)
+        .then(results => {
+          model.post = results.data;
+        });
+    }
+
+    function updatePost(){
+      $http.patch (`/api/classifieds/${stateParams.id}`, model.post);
+    }
+
     }
 }());
